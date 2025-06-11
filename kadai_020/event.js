65b8940197f140735040a8a4b8c5a854e578157f
YoshiNalegra <happy.mail24alegria@gmail.com>
2025-06-11 21:45:53 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,17 @@
+// textというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// parent-listというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text')
+
+
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  
+
+  text.textContent = 'ボタンをクリックしました';
+
+ 
+
+});
